788b00e1bda2ea61a31bde09fabcef4c4cdfa595
kmgovind <kavin.gmk@gmail.com>
2021-12-24 17:55:35 -0500

    Completed Day 4

---
+++ aoc-2021/four/four-two.py
@@ -0,0 +1,65 @@
+import numpy as np
+import re
+
+
+class Board:
+    def __init__(self, nums):
+        self.board = [
+            [[nums[i][j], False] for j in range(5)]
+            for i in range(5)
+        ]
+
+    def checkWin(self):
+        for row in range(5):
+            if all([self.board[row][i][1] for i in range(5)]):
+                return True
+        for col in range(5):
+            if all([self.board[i][col][1] for i in range(5)]):
+                return True
+        return False
+    
+    def get_score(self, last_called):
+        uncalled_sum = 0
+        for row in range(5):
+            for col in range(5):
+                if not self.board[row][col][1]:
+                    uncalled_sum += self.board[row][col][0]
+
+        return uncalled_sum * last_called
+
+    def mark(self, num):
+        for row in self.board:
+            for el in row:
+                if el[0] == num:
+                    el[1] = True
+
+def parse_board(lines):
+    return [[int(i) for i in re.split(" +", line.strip())] for line in lines]
+
+with open("input.txt") as inputFile:
+    data = inputFile.read().strip().split("\n")
+
+    nums = [int(i) for i in data[0].split(",")]
+
+    boards = []
+    i = 2
+    while i < len(data):
+        x = parse_board(data[i:i+5])
+        boards.append(Board(x))
+        i += 6
+
+    winningOrder = []
+    for x in nums:
+        for i in range(len(boards)):
+            boards[i].mark(x)
+
+            if i not in winningOrder and boards[i].checkWin():
+                winningOrder.append(i)
+        
+
+        if len(winningOrder) == len(boards):
+            break
+    
+    ans = boards[winningOrder[-1]].get_score(x)
+    print(ans)
+        
